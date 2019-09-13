800eab73c7d4e91c7b023c178c3d4d8f1df73c49
Matt Killian <31752429+killianmh@users.noreply.github.com>
2019-09-13 09:46:28 -0700

    Header revision (#5)
    * added accessible styling to :focus
    
    * replaced old header; removed dependency on foundation classes; added dropdown support divs; added ul/li templates for easy reference

--- js/nav.js
+++ js/nav.js
@@ -1,64 +1,58 @@
+// Helper functions
 
- $('body').on('click','[data-menuid]', function() {
-    $(this).toggleClass('active');
-})
+// Add "add-scroll" and remove "no-scroll" class
+function add_scroll_to_mobile() {
+    var $nav = $('body').find('[data-sv-mainnav]');
+    var height = $(window).height();
 
-// Mobile Nav
+    $nav.height(height - 67);  // minus height of nav bar
+    $nav.addClass('add-scroll');
+    $nav.removeClass('no-scroll');
+    $('body').addClass('nav-open');
+}
 
-     $('body').on('click','.hamburger', function() {
-        $(this).toggleClass('active');
-        $(this).parent().toggleClass('active');
-        $('body').find('[data-sv-mainnav]').toggleClass('active');
-    });
+// Add "no-scroll" and remove "add-scroll" class
+function remove_scroll() {
+    var $nav = $('body').find('[data-sv-mainnav]');
 
-
-     $('body').on('click','[data-menu-toggle-parent]',function(){
-        $(this).toggleClass('active');
-        $(this).parent().find('.mobile-children').slideToggle();
-        $(this).parent().find('.parent-arrow').toggleClass('open');
-        $(this).parent().toggleClass('selected');
-        if($(this).parent().find('.parent-arrow').hasClass('open') || $(this).parent().siblings().find('.parent-arrow').hasClass('open')){
-            add_scroll_to_mobile();
-        }else{
-            remove_scroll();
-        }
-    });
-
-     $('body').on('click','[data-menu-toggle-item]',function(){
-        $(this).next().slideToggle();
-        $(this).toggleClass('open');
-        $(this).prev('div').toggleClass('selected');
-    });
-
-    function add_scroll_to_mobile(){
-        var $nav =  $('body').find('[data-sv-mainnav]');
-        var height = $( window ).height();
-
-        $nav.height(height - 67);  // minus height of nav bar
-        $nav.addClass('add-scroll');
-        $nav.removeClass('no-scroll');
-        $('body').addClass('nav-open');
-    }
-
-    function remove_scroll(){
-        var $nav = $('body').find('[data-sv-mainnav]');
-
-        $nav.removeClass('add-scroll');
-        $nav.addClass('no-scroll');
-        $('body').removeClass('nav-open');
-    }
+    $nav.removeClass('add-scroll');
+    $nav.addClass('no-scroll');
+    $('body').removeClass('nav-open');
+}
 
 
 // Wait for when header and footer load, then attach javaScript event listeners
 // DO NOT USE THIS CODE FOR SHELL SETUP
-// Replace this code with document.ready
+// Replace this event listener with document.ready
 
 // header loaded
-document.addEventListener("headerImported", function(event){
+document.addEventListener("headerImported", function (event) {
     console.log("header loaded")
+
+    // Desktop
+
+        // Toggle submenu open on indicator click
+        $('[data-menu-toggle]').on('click', function () {
+            $(this).toggleClass('active');
+        })
+
+    // Mobile Nav
+
+        // Toggle mobile dropdown on hamburger menu btn click
+        $('[data-mobilenav-toggle]').on('click', function () {
+            $(this).toggleClass('active');
+            $(this).parent().parent().toggleClass('active');
+        });
+
+        // Toggle submenu open on indicator click
+        $('[data-menu-toggle-mobile]').on('click', function () {
+            $(this).toggleClass('active');
+            $(this).parent().parent().toggleClass('active');
+            $(this).parent().parent().find('ul').slideToggle();
+        });
 })
 
 // footer loaded
-document.addEventListener("footerImported", function(event){
+document.addEventListener("footerImported", function (event) {
     console.log("footer loaded")
 })
