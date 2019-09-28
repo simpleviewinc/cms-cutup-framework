b061fedb3bf47cc9eac7aa4e8abcc708344f0d5c
Matt Killian <31752429+killianmh@users.noreply.github.com>
2019-09-27 17:15:23 -0700

    Menu accessibility (#6)
    * added script for adding buttons to nav to allow for accessible tabbing through menu
    
    * simple header styling and additional accessible functionality to primary dropdowns
    
    * remove unnecessary css media query

--- js/nav.js
+++ js/nav.js
@@ -36,6 +36,34 @@ document.addEventListener("headerImported", function (event) {
             $(this).toggleClass('active');
         })
 
+        // add buttons to nav menu items to allow screen readers to accessdropdowns
+        // add appropriate aria-labels to button and parent link
+        var root = $(".header-section");
+        var menuItems = root.find(".has-dropdown, .has-submenu");
+    
+        menuItems.each(function(i, el){
+            var activatingA = $(el).children(".item-inner").children("a")
+            
+            var dropdown = $(el).children(".item-inner").next();
+            dropdown.attr("id", "child-" + i + "-dropdown")
+            var btn = "<button class='sr_only' aria-label='toggle submenu for " + activatingA.text() + "' aria-controls='child-" + i + "-dropdown'></button>";
+            activatingA.after(btn);
+
+            $(el).find("button").click(function(e){
+                var parent = $(this).parent().parent();
+                if(parent.hasClass("active")){
+                    parent.removeClass("active");
+                    activatingA.attr("aria-expanded", "false");
+                    $(this).attr("aria-expanded", "false");
+                } else {
+                    parent.addClass("active");
+                    activatingA.attr("aria-expanded", "true");
+                    $(this).attr("aria-expanded", "true");
+                }
+            })
+
+        })
+
     // Mobile Nav
 
         // Toggle mobile dropdown on hamburger menu btn click
